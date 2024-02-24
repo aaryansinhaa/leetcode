18e652692557c15ff020444d773af375d0f2067c
Aaryan Kumar Sinha <138802020+aaryansinhaa@users.noreply.github.com>
2024-02-24 21:56:43 +0530

    did bruteforce on twosum

--- twosum.java
+++ twosum.java
@@ -1,13 +1,22 @@
 import java.util.*;
 class Solution {
     public static int[] twoSum(int[] nums, int target) {
-        int[] index = new int[2];
-        int index1[];
-        Arrays.sort(nums);
-        
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        return null;
 
         
-        return index;
         
+        
+    }
+    public static void main(String[] args){
+        int[] nums = {10,2,13,14};
+        int [] index = twoSum(nums, 12);
+        System.out.println(index);
     }
 }
